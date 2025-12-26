9a8450c44f1bcc9869bc14376040ea1496cb2129
kee <chinboy2012@gmail.com>
2025-12-26 18:08:22 +0800

    auth listener

--- src/Events/Listener.php
+++ src/Events/Listener.php
@@ -2,8 +2,8 @@
 
 namespace Lx\Events;
 
-
 use Attribute;
+use ReflectionClass;
 
 #[Attribute(Attribute::IS_REPEATABLE | Attribute::TARGET_ALL)]
 class Listener
@@ -16,4 +16,32 @@ class Listener
     //
   }
 
+  static function parseCallback($event) {
+    $listeners = [];
+    $reflection = new ReflectionClass($event);
+    $attr = $reflection->getAttributes(Listener::class);
+    if (count($attr) > 0) {
+      foreach ($attr as $a) {
+        $ins = $a->newInstance();
+        if ($listener = $ins->listener) {
+          if (
+            (is_string($listener) && class_exists($listener))
+            || is_callable($listener)
+          ) {
+            if ($ins->method && method_exists($listener, $ins->method)) {
+              if (is_callable([$listener, $ins->method])) {
+                $listener = fn($event) => call_user_func([$listener, $ins->method], $event);
+              }
+              else {
+                $listener = [$listener, $ins->method];
+              }
+            }
+            $listeners[] = $listener;
+
+          }
+        }
+      }
+    }
+    return $listeners;
+  }
 }
